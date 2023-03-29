83eddf825415bf6dbd16afe2615c36917f816c37
Erkan <ErkanOzulu@users.noreply.github.com>
2023-03-29 23:56:14 +0300

    tags = "@wip"

--- src/test/java/com/cydeo/runners/SymundRunner.java
+++ src/test/java/com/cydeo/runners/SymundRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         stepNotifications = true,
-        tags = "@SYMU10-544",
+        tags = "@wip",
         publish=true
 )
 
