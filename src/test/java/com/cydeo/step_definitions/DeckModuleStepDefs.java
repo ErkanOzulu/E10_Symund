09b5d54d229068f2d603f0e38b481e0631ccc03f
İbrahim ANAYURT <ibrahim_anayurt@hotmail.com>
2023-03-30 16:42:48 +0300

    Deck feature

---
+++ src/test/java/com/cydeo/step_definitions/DeckModuleStepDefs.java
@@ -0,0 +1,37 @@
+package com.cydeo.step_definitions;
+
+import com.cydeo.pages.DeckPage;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class DeckModuleStepDefs {
+
+    DeckPage deckPage = new DeckPage();
+
+    @Given("user navigates to deck")
+    public void user_navigates_to_deck() {
+
+    }
+    @When("User can see the three-line button and click it")
+    public void user_can_see_the_three_line_button_and_click_it() {
+
+    }
+    @When("User see the opened sidebar")
+    public void user_see_the_opened_sidebar() {
+
+    }
+    @When("User see the add-board button and click it")
+    public void user_see_the_add_board_button_and_click_it() {
+
+    }
+    @When("User sees the popup box and enters the <{string}>")
+    public void user_sees_the_popup_box_and_enters_the(String string) {
+
+    }
+    @Then("User can see the created <{string}> on the side bar")
+    public void user_can_see_the_created_on_the_side_bar(String string) {
+
+    }
+
+}
---
+++ src/test/resources/features/Deck.feature
@@ -0,0 +1,23 @@
+Feature: Deck Module
+  As a user, I should be able to manage my works by creating a
+  new board/list/card and modifying them under Deck Module.
+
+  Background: user is expected to be on the dashboard page
+    Given user is logged in and on the dashboard page
+    And user navigates to "deck"
+
+  @deck
+  Scenario Outline: User can create a new board
+    When User can see the three-line button and click it
+    And User see the opened sidebar
+    And User see the add-board button and click it
+    And User sees the popup box and enters the <"board name">
+    Then User can see the created <"board name"> on the side bar
+
+    Examples:
+      | board name  |
+      | Galatasaray |
+      | New board   |
+      | New123      |
+      | New 123     |
+      | 123 new     |
