a263a1dbeb833a0806fc3f9c21c29e6808a1a097
Erkan <ErkanOzulu@users.noreply.github.com>
2023-04-01 23:48:04 +0300

    Located deleted files elements

---
+++ src/test/java/com/cydeo/pages/DeletedFilesPage.java
@@ -0,0 +1,30 @@
+package com.cydeo.pages;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class DeletedFilesPage extends FilesPageE {
+
+    public DeletedFilesPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+
+
+    @FindBy(xpath = "//div[@id='app-content-trashbin']/div[3]/h2")
+    public WebElement noDeletedFiles;
+
+    @FindBy(xpath = "//div[@id='app-content-trashbin']//span[@class='innernametext']")
+    public List<WebElement> alldeletedFilesName;
+
+   @FindBy(xpath = " //div[@id='app-content-trashbin']//td[@class='date']/span")
+   public List<WebElement> alldeletedFilesDate;
+
+    @FindBy(xpath = "//div[@id='app-content-trashbin']/table/thead/tr/th")
+    public List<WebElement>order;
+
+}
