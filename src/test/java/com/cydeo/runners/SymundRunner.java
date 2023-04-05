1c5848d6e430c50b40b5c40a82222929e11956c1
Erkan <ErkanOzulu@users.noreply.github.com>
2023-04-05 14:46:05 +0300

    stepNotifications = true, added again

--- src/test/java/com/cydeo/runners/SymundRunner.java
+++ src/test/java/com/cydeo/runners/SymundRunner.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources/features",
         glue = "com/cydeo/step_definitions",
         dryRun = false,
+        stepNotifications = true,
         tags = "@wip",
         publish=true
 )
