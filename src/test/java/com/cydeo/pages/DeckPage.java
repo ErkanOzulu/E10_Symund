0a198094b48992edfd0b7ae5dcbc1da666e5a167
İbrahim ANAYURT <ibrahim_anayurt@hotmail.com>
2023-04-05 11:03:58 +0300

    Deck feature TC-3 completed

--- src/test/java/com/cydeo/pages/DeckPage.java
+++ src/test/java/com/cydeo/pages/DeckPage.java
@@ -3,9 +3,15 @@ package com.cydeo.pages;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import org.junit.Assert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class DeckPage extends BasePage{
 
@@ -26,7 +32,7 @@ public class DeckPage extends BasePage{
     @FindBy(xpath = "//input[@class=\"icon-confirm\"]")
     public WebElement inputBoardNameSubmitButton;
 
-    @FindBy(xpath = "(//button[@class=\"action-item action-item--single icon-add undefined undefined has-tooltip\"])[1]")
+    @FindBy(xpath = "//div[@id=\"stack-add\"]/button")
     public WebElement addListButton;
 
     @FindBy(xpath = "//input[@id=\"new-stack-input-main\"]")
@@ -38,7 +44,7 @@ public class DeckPage extends BasePage{
 
 
 
-    public void createBoardBeforeScenarioDeckModule(String str){
+    public void createBoardBeforeScenarioDeckModule(String boardName){
 
         DeckPage deckPage = new DeckPage();
         LoginPage loginPage=new LoginPage();
@@ -50,20 +56,65 @@ public class DeckPage extends BasePage{
         deckPage.threeLineButton.click();
         BrowserUtils.sleep(3);
         System.out.println("firstBoard.getText() = " + firstBoard.getText());
-        boolean created = firstBoard.getAttribute("title").contains(str);
+        boolean created = firstBoard.getAttribute("title").contains(boardName);
         if ( created ) {
             firstBoard.click();
         }else{
             deckPage.addBoardButton.click();
-            deckPage.inputBoardNameBox.sendKeys(str);
+            BrowserUtils.sleep(2);
+            deckPage.inputBoardNameBox.sendKeys(boardName);
             deckPage.inputBoardNameSubmitButton.click();
             Driver.getDriver().navigate().refresh();
             BrowserUtils.sleep(3);
             firstBoard.click();
         }
+    }
 
+    public boolean listNameIsDisplayed(String listName) {
+        String xpathForList = "//h3[contains(text(), '" + listName + "')]";
+        try {
+            boolean listNameCreatedBefore = Driver.getDriver().findElement(By.xpath(xpathForList)).getText().contains(listName);
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public void createdMainTeamListBeforeScenario(String listName){
+
+        DeckPage deckPage = new DeckPage();
+        String addCardButtonLocatorText = "//h3[contains(text(), '" + listName + "')]/../button";
+
+        if (listNameIsDisplayed(listName)){
+            WebElement addCardButton = Driver.getDriver().findElement(By.xpath(addCardButtonLocatorText));
+            addCardButton.click();
+        }else {
+            deckPage.addListButton.click();
+            deckPage.addListNameBox.sendKeys(listName + Keys.ENTER);
+            Driver.getDriver().navigate().refresh();
+            BrowserUtils.sleep(3);
+            WebElement addCardButton = Driver.getDriver().findElement(By.xpath(addCardButtonLocatorText));
+            addCardButton.click();
+        }
+    }
+
+    public void createCardName(String listName, String cardName){
+
+        String enterCardNameBoxText = "((//h3[contains(text(),'" + listName + "')]/../../div)[2]//input)[1]";
+        WebElement enterCardNameBoxUnderRightListBox = Driver.getDriver().findElement(By.xpath(enterCardNameBoxText));
+        enterCardNameBoxUnderRightListBox.sendKeys(cardName + Keys.ENTER);
+    }
+
+    public void cardNameCreatedUnderRightListVerify(String cardName, String listName){
+
+        String cardNameUnderListNameText = "//span[contains(text(),'"+ cardName +"')]/../../../../../../../div/h3";
+        WebElement cardNameUnderListName = Driver.getDriver().findElement(By.xpath(cardNameUnderListNameText));
+        Assert.assertTrue(cardNameUnderListName.getText().contains(listName));
 
     }
+
+
+
 
 
 }
