72cef4fa411338d62ec5fcc58bdc33d198d8bd66
yakup öztürk <yakupozturk29@hotmail.com>
2023-04-12 10:16:34 +0300

    I added junit report plugin

--- src/test/java/com/cydeo/runners/SymundRunner.java
+++ src/test/java/com/cydeo/runners/SymundRunner.java
@@ -17,7 +17,7 @@ import org.junit.runner.RunWith;
         glue = "com/cydeo/step_definitions",
         dryRun = false,
         stepNotifications = true,
-        tags = "@wip",
+        tags = "@smoke",
         publish=true
 )
 
