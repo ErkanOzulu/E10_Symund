7fce0fae6bc4afcda847b39cc0b6392eecf26bb0
Osman Oğuz <osman-oguz@hotmail.com>
2023-04-15 12:40:23 +0300

    Completed CalendarPage @SYMU10-514 for the AC 4-5

--- src/test/java/com/cydeo/step_definitions/CalendarStepDefs.java
+++ src/test/java/com/cydeo/step_definitions/CalendarStepDefs.java
@@ -5,15 +5,25 @@ import com.cydeo.pages.LoginPage;
 import com.cydeo.utilities.BrowserUtils;
 import com.cydeo.utilities.ConfigurationReader;
 import com.cydeo.utilities.Driver;
+import com.github.javafaker.Faker;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+
+
 
 public class CalendarStepDefs {
 
     LoginPage loginPage = new LoginPage();
     CalendarPage calendarPage = new CalendarPage();
+    int beforeDeletingSize;
+    int afterDeletingSize;
+
+
 
     @Given("user is on the calender page")
     public void user_is_on_the_calender_page() {
@@ -26,7 +36,7 @@ public class CalendarStepDefs {
 
     @When("user clicks the menu toggle")
     public void user_clicks_the_menu_toggle() {
-        BrowserUtils.waitFor(10);
+        BrowserUtils.waitFor(3);
         calendarPage.menuToggleViewButton.click();
     }
 
@@ -36,8 +46,8 @@ public class CalendarStepDefs {
         calendarPage.dayButton.click();
     }
 
-    @Then("user should see the daily calender view")
-    public void user_should_see_the_daily_calender_view() {
+    @Then("verify that user should see the daily calender view")
+    public void verify_that_user_should_see_the_daily_calender_view() {
         Assert.assertEquals("Day", calendarPage.selectedButton.getText());
 
     }
@@ -47,8 +57,8 @@ public class CalendarStepDefs {
         calendarPage.weekButton.click();
     }
 
-    @Then("user should see the weekly calender view")
-    public void user_should_see_the_weekly_calender_view() {
+    @Then("verify that user should see the weekly calender view")
+    public void verify_that_user_should_see_the_weekly_calender_view() {
         Assert.assertEquals("Week", calendarPage.selectedButton.getText());
     }
 
@@ -57,9 +67,92 @@ public class CalendarStepDefs {
         calendarPage.monthButton.click();
     }
 
-    @Then("user should see the monthly calender view")
-    public void user_should_see_the_monthly_calender_view() {
+    @Then("verify that user should see the monthly calender view")
+    public void verify_that_user_should_see_the_monthly_calender_view() {
         Assert.assertEquals("Month", calendarPage.selectedButton.getText());
+    }
+
+    @When("user clicks New event button")
+    public void user_clicks_new_event_button() {
+        calendarPage.newEventButton.click();
+    }
+
+    @When("user clicks the save button")
+    public void user_clicks_the_save_button() {
+       BrowserUtils.waitFor(4);
+        calendarPage.saveButton.click();
+    }
+
+
+
+    @Then("verify that user should see <{string}> on the related day through the Monthly Calendar view")
+    public void verify_that_user_should_see_on_the_related_day_through_the_monthly_calendar_view(String string) {
+
+        for (WebElement eachEventTitle : calendarPage.eventTitleContainer) {
+            System.out.println("eachEventTitle.getText() = " + eachEventTitle.getText());
+            if (eachEventTitle.getText().equals(string)){
+                Assert.assertEquals(string,eachEventTitle.getText());
+                break;
+            }
+
+        }
+
+
+    }
+
+    @When("user clicks the Personal button if it is unselected")
+    public void user_clicks_the_personal_button_if_it_is_unselected() {
+        if (!(calendarPage.personalCheckBox.isSelected())){
+            calendarPage.personalCheckBox.click();
+        }
+
+    }
+
+    @When("user fill <{string}> in the event name, {string} from date and {string} to date")
+    public void user_fill_in_the_event_name_from_date_and_to_date(String string, String string2, String string3) {
+        // Firstly we clear the box and later send keys for date
+        calendarPage.inputDateFrom.sendKeys(Keys.chord(Keys.CONTROL, "a")+Keys.DELETE +string2);
+        calendarPage.inputDateTo.sendKeys(Keys.chord(Keys.CONTROL, "a")+Keys.DELETE+ string3);
+        calendarPage.eventTitleInputBox.sendKeys(string);
+    }
+
+
+    @When("user clicks on the event which is on <{string}> day")
+    public void user_clicks_on_the_event_which_is_on_day(String string) {
+
+        // event number before deleting the event
+        beforeDeletingSize = calendarPage.eventTitleContainer.size();
+        System.out.println("beforeDeletingSize = " + beforeDeletingSize);
+
+         calendarPage.eventTitle.click();
+    }
+    @When("user clicks more option and toggle to expand menu")
+    public void user_clicks_more_option_and_toggle_to_expand_menu() {
+
+
+        calendarPage.moreButton.click();
+
+        calendarPage.menuToggleForDelete.click();
+    }
+    @Then("verify that user can delete that event")
+    public void verify_that_user_can_delete_that_event() {
+
+
+
+        // we delete the event
+        calendarPage.deleteButton.click();
+        BrowserUtils.waitFor(2);
+
+
+       // after deleting the event we check the number of events but before that we have to refresh the page
+        Driver.getDriver().navigate().refresh();
+        BrowserUtils.waitFor(10);
+
+
+        afterDeletingSize = calendarPage.eventTitleContainer.size();
+        System.out.println("afterDeletingSize = " + afterDeletingSize);
+
+        Assert.assertTrue(afterDeletingSize + 1 == beforeDeletingSize);
     }
 
 
--- src/test/resources/features/Calendar_OO.feature
+++ src/test/resources/features/Calendar_OO.feature
@@ -1,4 +1,4 @@
-@SYMU10-514
+@SYMU10-514 @wip
 Feature: Calender Functionality
   As a user, I should be able to see
   Daily-Weekly-Monthly calendar view
@@ -6,18 +6,53 @@ Feature: Calender Functionality
 
   Background: user is logged in and on the calender page
     Given user is on the calender page
+
   @SYMU10-584
   Scenario: User can display daily calendar view
     When user clicks the menu toggle
     And user clicks the day link
-    Then user should see the daily calender view
+    Then verify that user should see the daily calender view
+
   @SYMU10-585
   Scenario: User can display weekly calendar view
     When user clicks the menu toggle
     And user clicks the week link
-    Then user should see the weekly calender view
+    Then verify that user should see the weekly calender view
+
   @SYMU10-586
   Scenario: User can display monthly calendar view
     When user clicks the menu toggle
     And user clicks the month link
-    Then user should see the monthly calender view
+    Then verify that user should see the monthly calender view
+
+
+  @SYMU10-602
+  Scenario Outline: User can create a new event under the Calendar module and see it on the related day through the Monthly Calendar view
+    When user clicks New event button
+    And user fill <"Osman"> in the event name, "<FROM DATE>" from date and "<TO DATE>" to date
+    And user clicks the save button
+    And user clicks the menu toggle
+    And user clicks the month link
+    And user clicks the Personal button if it is unselected
+    Then verify that user should see <"Osman"> on the related day through the Monthly Calendar view
+
+    Examples:
+      | FROM DATE                   | TO DATE                   |
+      | from 04/08/2023 at 05:00 AM | to 04/08/2023 at 06:00 AM |
+      | from 04/09/2023 at 05:00 AM | to 04/08/2023 at 06:00 AM |
+      | from 04/08/2023 at 06:00 AM | to 04/08/2023 at 05:00 AM |
+      | from 05/09/2023 at 05:00 AM | to 04/08/2023 at 06:00 AM |
+      | from 05/09/2023 at 05:00 AM |                           |
+      |                             | to 04/08/2023 at 06:00 AM |
+      |                             |                           |
+
+  @SYMU10-603
+  Scenario: User can delete any event through the Monthly Calendar view by clicking on the event and then using “more” option
+    When user clicks the menu toggle
+    And user clicks the month link
+    And user clicks the Personal button if it is unselected
+    And user clicks on the event which is on <"04/08/2023"> day
+    And user clicks more option and toggle to expand menu
+    Then verify that user can delete that event
+
+
